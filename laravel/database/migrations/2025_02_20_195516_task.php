808685bce9596a41a5069be598f32626c46839c5
Aluno5 <henriquehenriqueshenrico@gmail.com>
2025-02-20 17:25:59 -0300

    adicionando a migration task

--- laravel/database/migrations/2025_02_20_195516_task.php
+++ laravel/database/migrations/2025_02_20_195516_task.php
@@ -11,7 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('tb_tag', function(Blueprint $table){
+            $table->id('id_tag');
+            $table->string('nm_tag', 45);
+            $table->foreignId('id_usuario')->references('id_usuario')->on('tb_usuario')->onDelete('cascade');
+        });
+        Schema::create('tb_task', function(Blueprint $table){
+            $table->id('id_task');
+            $table->string('nm_task', 45);
+            $table->longtext('nm_task', 500);
+            $table->boolean('ic_done');
+            $table->foreignId('id_tag')->references('id_tag')->on('tb_tag')->onDelete('cascade');
+        });
     }
 
     /**
@@ -19,6 +30,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tb_tag');
+        Schema::dropIfExists('tb_task');
     }
 };
