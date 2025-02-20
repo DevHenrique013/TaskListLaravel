7baa3d6f23829f4ec1736a4632337092eba62e28
Aluno5 <henriquehenriqueshenrico@gmail.com>
2025-02-20 17:36:16 -0300

    comitando

--- laravel/database/migrations/2025_02_20_195531_usuario.php
+++ laravel/database/migrations/2025_02_20_195531_usuario.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('tb_classe', function(Blueprint $table){
+            $table->id('id_classe');
+            $table->string('nm_classe', 45);
+            $table->foreignId('id_curso')->references('id_curso')->on('tb_curso')->onDelete('cascade');
+        });
     }
 
     /**
